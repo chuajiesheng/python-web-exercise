fb74ad83b56a3489efe6ebf1ad81024d3a205234
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-06-26 03:56:33 -0400

    added bootstrap and add function

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,20 +7,27 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
 db = SQLAlchemy(app)
-db.create_all()
 
 class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	text = db.Column(db.String)
-	done = db.Column(db.Boolean)
+	done = db.Column(db.Boolean, default=False)
 
 def add_todo(text):
-	db.session.add(Item(text=text))
+	db.session.add(Todo(text=text))
 	db.session.commit()
 	print 'adding todo:', text
 
+db.create_all()
 @app.route('/')
 def homepage():
 	return render_template('home.html', app_name=';/', author='Helios')
+
+@app.route('/add', methods=('GET', 'POST'))
+def add():
+	if request.method == 'POST':
+		taskname = request.form['taskname']
+		add_todo(taskname)
+	return render_template('add.html')
 
 app.run()
